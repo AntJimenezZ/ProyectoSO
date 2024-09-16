3369092021f1e0eba966632457179ab7f75a5020
noni <noni45100@hotmail.com>
2024-09-16 13:15:50 -0600

    first commit

---
+++ video-player/pages/test.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const cargarCancion = () => {
+  console.log('Cargando canción...');
+  const video = document.querySelector('video');
+  video.play();
+}
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <video class="border-2 border-gray-500 w-full max-w-6xl" controls>
+      <source src="http://localhost:8080/song" type="video/mp4">
+      Tu navegador no soporta la etiqueta de video.
+    </video>
+  </div>
+</template>
+
+<style scoped>
+/* Puedes agregar estilos aquí si es necesario */
+</style>
